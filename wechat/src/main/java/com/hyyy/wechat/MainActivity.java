a3cc6ce794124f3a0dd5f54aad9dd0fb76c2582f
castial <897257524@qq.com>
2015-08-22 21:20:42 +0800

    change wechat

--- wechat/src/main/java/com/hyyy/wechat/MainActivity.java
+++ wechat/src/main/java/com/hyyy/wechat/MainActivity.java
@@ -1,6 +1,10 @@
 package com.hyyy.wechat;
 
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,20 +13,62 @@ import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.Window;
 
+import com.hyyy.wechat.fragment.TabFragment;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+
+    private ViewPager mViewPager;
+
+    private List<Fragment> mFragments = new ArrayList<Fragment>();
+
+    private String[] mTitles = new String[]{
+            "First Fragment", "Second Fragment", "Third Fragment", "Fourth Fragment"
+    };
+
+    private FragmentPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         setOverflowButtonAlways();
+        getActionBar().setDisplayShowHomeEnabled(false);//去掉actionbar的logo
 
-        //去掉actionbar的logo
-        getActionBar().setDisplayShowHomeEnabled(false);
+        initView();
+        initData();
+        mViewPager.setAdapter(mAdapter);
+
+    }
+
+    private void initData() {
+        for (String title : mTitles){
+            TabFragment tabFragment = new TabFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString(TabFragment.TITLE, title);
+            tabFragment.setArguments(bundle);
+            mFragments.add(tabFragment);
+        }
+
+        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mFragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragments.size();
+            }
+        };
+    }
+
+    private void initView() {
+        mViewPager = (ViewPager) findViewById(R.id.wechat_viewpager);
     }
 
     @Override

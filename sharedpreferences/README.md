f44603ed985325e9fe6dac2e46056bdf2e51d095
castial <897257524@qq.com>
2015-08-25 22:59:05 +0800

    add sharedPreferences

---
+++ sharedpreferences/README.md
@@ -0,0 +1,15 @@
+###[SharedPreferences的读写操作](https://github.com/castial/android-samples/tree/master/sharedpreferences)
+>sharedpreferences的简单使用
+
+非常简单，sharedferences的读写操作，android基础入门教程。
+###开发环境
+IDE --> Android Studio 1.2  
+Android测试版本 --> Android 4.4.4  
+模拟器 --> Genymotion
+
+###使用方法
+打开Android Studio，直接下载此项目，导入即可。  
+
+另外，如果只需要看个别module的代码，请下载module，在Android Studio中选择import module即可。
+###知识点总结
+* SharedPreferences的读写操作
---
+++ sharedpreferences/src/main/java/com/hyyy/sharedpreferences/MainActivity.java
@@ -0,0 +1,75 @@
+package com.hyyy.sharedpreferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private EditText editUsername;
+    private EditText editPassword;
+    private Button btnWrite;
+    private Button btnRead;
+    private TextView showUsername;
+    private TextView showPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        intitView();
+
+        sharedPreferences = this.getSharedPreferences("user", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
+
+    private void intitView() {
+        editUsername = (EditText) findViewById(R.id.username);
+        editPassword = (EditText) findViewById(R.id.password);
+        showUsername = (TextView) findViewById(R.id.show_username);
+        showPassword = (TextView) findViewById(R.id.show_password);
+        btnRead = (Button) findViewById(R.id.write);
+        btnRead.setOnClickListener(this);
+        btnWrite = (Button) findViewById(R.id.read);
+        btnWrite.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.write:
+                String username = editUsername.getText().toString();
+                String password = editPassword.getText().toString();
+                if ("".equals(username) || "".equals(password)) {
+                    Toast.makeText(MainActivity.this, "Please input username or password!", Toast.LENGTH_SHORT).show();
+                } else {
+                    editor.putString("username", username);
+                    editor.putString("password", password);
+                    Toast.makeText(MainActivity.this, "Write Successfully!", Toast.LENGTH_SHORT).show();
+                }
+
+                editor.commit();
+                break;
+
+            case R.id.read:
+                String name = sharedPreferences.getString("username", null);
+                String pass = sharedPreferences.getString("password", null);
+                if ("".equals(name) || "".equals(pass)) {
+                    Toast.makeText(MainActivity.this, "The user.xml is null,PLease write it!", Toast.LENGTH_SHORT).show();
+                } else {
+                    showUsername.setText(name);
+                    showPassword.setText(pass);
+                }
+        }
+    }
+}
